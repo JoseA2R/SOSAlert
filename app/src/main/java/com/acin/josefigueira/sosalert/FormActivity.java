30879887b1cb69b45deaa1a52b0bb4cfed324990
JoseA2R <joseagustindrf@gmail.com>
2018-03-26 17:38:45 +0100

    do some changes

---
+++ app/src/main/java/com/acin/josefigueira/sosalert/FormActivity.java
@@ -0,0 +1,22 @@
+package com.acin.josefigueira.sosalert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by jose.figueira on 26-03-2018.
+ */
+
+public class FormActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userdata);
+
+    }
+
+}
+
--- app/src/main/java/com/acin/josefigueira/sosalert/MainActivity.java
+++ app/src/main/java/com/acin/josefigueira/sosalert/MainActivity.java
@@ -1,13 +1,33 @@
 package com.acin.josefigueira.sosalert;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button _btn = findViewById(R.id.app_start_btn);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+
+
+
+        _btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,FormActivity.class));
+            }
+        });
+
+    }
+
+    public void onClick(View view) {
     }
 }
