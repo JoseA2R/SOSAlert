4249ed50c063836903d753131cc2abe11f3dd437
JoseA2R <joseagustindrf@gmail.com>
2018-04-02 16:54:04 +0100

    prueba geolocalizacion

--- app/src/main/java/com/acin/josefigueira/sosalert/View/SOSActivity.java
+++ app/src/main/java/com/acin/josefigueira/sosalert/View/SOSActivity.java
@@ -2,6 +2,7 @@ package com.acin.josefigueira.sosalert.View;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -20,6 +21,8 @@ import android.widget.Toast;
 
 import com.acin.josefigueira.sosalert.R;
 
+import static com.acin.josefigueira.sosalert.View.GPSPruebActivity.REQUEST_LOCATION;
+
 /**
  * Created by jose.figueira on 27-03-2018.
  */
@@ -28,8 +31,12 @@ public class SOSActivity extends AppCompatActivity {
 
     Button button;
     TextView textView;
+    Location location;
     LocationManager locationManager;
     LocationListener listener;
+
+    private TextView txtLongitude;
+    private TextView txtLatitude;
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -39,7 +46,34 @@ public class SOSActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.btnprueba);
         textView = (TextView) findViewById(R.id.tvprueba);
 
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        txtLongitude = (TextView) findViewById(R.id.txtLongitude);
+        txtLatitude = (TextView) findViewById(R.id.txtLatitude);
+
+        double longitude = 0.0;
+        double latitude = 0.0;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        } else{
+
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        }
+        if (location != null){
+
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+
+        }
+
+        txtLongitude.setText("Longitude: " + longitude);
+        txtLatitude.setText("Latitude: " +latitude);
+
+       /* locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         listener = new LocationListener() {
 
@@ -67,12 +101,12 @@ public class SOSActivity extends AppCompatActivity {
 
         };
 
-        configure_button();
+        configure_button();*/
 
     }
 
 
-    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) {
+   /*public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case 10:
 
@@ -105,13 +139,13 @@ public class SOSActivity extends AppCompatActivity {
                                 Manifest.permission.INTERNET}, 10);
                     }
                     return;
-                }*/
+                }
                 locationManager.requestLocationUpdates("gps", 10000, 0, listener);
 
                 }
             });
 
-        }
+        }*/
 }
 
 
